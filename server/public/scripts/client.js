3049b43a751418f4cedaceafbb51044110596923
Ian Lambert <lambe347@umn.edu>
2021-04-19 11:05:07 -0500

    starting on client.js, going back to server for a bit

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,22 @@ $( document ).ready( onReady );
 
 function onReady() {
     console.log('DOM ready');
+    $('#addJokeButton').on('click', addJoke);
+}
+
+function addJoke(){
+    let jokeObject = {
+        whoseJoke: $('#whoseJokeIn').val(),
+        question: $('#questionIn').val(),
+        punchLine: $('#punchlineIn').val(),
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/jokes',
+        data: jokeObject,
+    })
+        .then (function(resposne){
+            getJokes();
+        })
+        //do .catch later
 }

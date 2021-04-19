326b8221975cbd08ec93707d02b12612aeb21b6d
Ian Lambert <lambe347@umn.edu>
2021-04-19 12:06:15 -0500

    added comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,44 +1,55 @@
 console.log('client.js sourced');
 
 $( document ).ready( onReady );
-
+//function to ready the DOM for manipulation
 function onReady() {
     console.log('DOM ready');
+    //add handler for clicking on the add joke button
     $('#addJokeButton').on('click', addJoke);
+    //calling the getJokes function to have all the jokes already on the server displayed when the DOM is loaded.
     getJokes();
 }
-
+//function to add a joke to the server's data
 function addJoke(){
+    //takes the values of the inputs from the three input fields and adds them to properties in a new object.
     let jokeObject = {
         whoseJoke: $('#whoseJokeIn').val(),
         jokeQuestion: $('#questionIn').val(),
         punchLine: $('#punchlineIn').val(),
     }
+    //ajax method to post the new object to the server using the jokeObject created above as the data to be sent and stored.
     $.ajax({
         method: 'POST',
         url: '/jokes',
         data: jokeObject,
     })
+        //once done, receive back the data from the server to get the new joke onto the server.
         .then (function(resposne){
             getJokes();
         })
+        //error function in the event that a joke could not be added to the server.
         .catch(function(error){
             console.log('Error from server', error);
             alert('Sorry, could not add joke, try again later');
         })
-
+        //calls the clear input functions once the above is done to clear inputs for the next entry. 
     clearInputs();
 }
 
+//function to get the jokes data from the server and append it to the DOM
 function getJokes(){
+    //ajax method to receive data from the server
     $.ajax({
         method: 'GET',
         url: '/jokes',
     })
+        //once done, then append that data to the DOM in a new div, with the appended 
+        //elements being the properties and key values from the jokes array on the server.
         .then (function(response){
             console.log('Response from the server', response);
             $('#outputDiv').empty();
             console.log('Adding jokes to the DOM');
+            //loop to move through the array and append each element to the dom
             for (let joke of response){
                 $('#outputDiv').append(`
                     <div class="joke">
@@ -49,11 +60,13 @@ function getJokes(){
                 `)
             }
         })
+        //error function in the even that the data could not be received.
         .catch(function(error){
             console.log('Error from server', error);
             alert('Sorry, could not get joke book, try again later');
         })
 }
+//function to clear the input fields.
 function clearInputs(){
     $('#whoseJokeIn').val('');
     $('#questionIn').val('');
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+//sets up express to be used by the server
 const express = require( 'express' );
 const app = express();
 const bodyParser = require( 'body-parser' );
@@ -5,7 +6,7 @@ const PORT = 5000;
 
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
-
+//array to hold the jokes on the server to be sent to and edited by the client.
 let jokes = [
   {
     whoseJoke: "Danny",
@@ -36,20 +37,23 @@ let jokes = [
 
 // serve back static files
 app.use(express.static('server/public'));
-
+//when requested by the client, send the jokes array
 app.get('/jokes', (req, res) => {
   res.send(jokes);
   console.log('Sending joke book', jokes);
 })
-
+//when requested by the client, receive a new joke object from the client's inputs
 app.post('/jokes', (req, res) => {
+  //declare new variable set to that request body
   let newJoke = req.body;
   console.log('Got new joke', newJoke);
+  //send back status indicating to the client that the object was created successfully.
   res.sendStatus(201);
+  //push the new joke object to the already existing array alongside the other already present jokes.
   jokes.push(newJoke);
 })
 
-
+//function to prepare the server to listen on the port, designated above as 5000.
 app.listen(PORT, () => {
   console.log('server running on: ', PORT);
 }); // end spin up server
